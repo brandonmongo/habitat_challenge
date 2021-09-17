072a1225e76885b2c58df15fa08fab514d609c51
brandonmongo <brandonmongo@hotmail.com>
2021-09-17 09:55:57 +0000

    added results view and url

--- index/views.py
+++ index/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import tender_result
 # Create your views here.
+
+
+def index(request):
+
+    tender_results = tender_result.objects.all()
+
+    context = {
+        "tender_results": tender_results,
+    }
+
+    return render(request, "index/index.html", context)
+
